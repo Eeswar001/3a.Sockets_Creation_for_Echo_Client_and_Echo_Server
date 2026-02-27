2fa94aa3ae164b649dd236e101502aca9d9b2eea
Eeswar001 <eswarkirba@gmail.com>
2026-02-27 14:08:42 +0530

    Succes

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = '127.0.0.1'  
+PORT = 65432  
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((HOST, PORT))
+
+    message = input("Enter a message to send to the server: ")
+    client_socket.sendall(message.encode('utf-8'))
+
+    data = client_socket.recv(1024)
+    print(f"Received echo: {data.decode('utf-8')}")
